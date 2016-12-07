25b4df2f9b34f31d68f661a45e589b4ac564c838
Kanishka Ragula <k.ragula1@gmail.com>
2016-12-07 14:12:56 -0700

    Add files via upload

---
+++ Code/EmpirMain.java
@@ -0,0 +1,8 @@
+package Main;
+import PeriodicTable.H;
+//import edu.bu.lite.periodictable.PeriodicTable;
+public class EmpirMain {
+	public static void main(String[] args){
+		H h = new H();
+	}
+}
